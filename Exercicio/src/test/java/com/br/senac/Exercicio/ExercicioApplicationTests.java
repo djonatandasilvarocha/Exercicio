5270f12965a9f8e177d7d32962d3f09efa8392ec
DJONATAN DA SILVA DA ROCHA <djonatan.rocha@alunos.sc.senac.br>
2026-02-19 20:13:23 -0300

    primeiro commit

---
+++ Exercicio/src/test/java/com/br/senac/Exercicio/ExercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.senac.Exercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
